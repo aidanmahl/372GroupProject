c32635b4465a89472daa1fa1b526a976a7b20631
Ashley Zenzola <175030924+azenzola8@users.noreply.github.com>
2025-09-21 11:53:28 -0500

    Updating project with UML data.

--- 372GroupProject/src/main/java/FoodItem.java
+++ 372GroupProject/src/main/java/FoodItem.java
@@ -1,27 +1,34 @@
 /**
- * The FoodItem class retrieves food item data and calculates the total price.
+ * FoodItem class.
  */
 public class FoodItem {
 	// Properties
 	private String name;
-	private double unitPrice;
 	private int quantity;
-	
-	// Getters
+	private float price;
+
+	// Getters and setters
 	public String getName() {
 		return name;
 	}
 
-	public double getUnitPrice() {
-		return unitPrice;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public int getQuantity() {
 		return quantity;
 	}
-	
-	// Methods
-	public double getTotalPrice() {
-		return quantity * unitPrice;
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}	
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
 	}
 }
