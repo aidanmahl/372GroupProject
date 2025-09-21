d8aef32676c7e2bd3822867c6a22594a85663a85
Ashley Zenzola <175030924+azenzola8@users.noreply.github.com>
2025-09-21 16:02:47 -0500

    Rough draft.

--- 372GroupProject/src/main/java/Parser.java
+++ 372GroupProject/src/main/java/Parser.java
@@ -1,4 +1,20 @@
+import java.util.*;
+import java.io.*;
 
 public class Parser {
-
+	public static Order parseOrder(String file, int orderId) {
+		String type = "";
+		long date = 0;
+		List<FoodItem> foodItems = new ArrayList<>();
+		
+		try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+			
+	}catch (IOException e) {
+		e.printStackTrace();
+		System.out.println("Error: " + e.getMessage());
+	}
+		
+        return new Order(orderId, type, date, foodItems);
+	}
 }
+		
