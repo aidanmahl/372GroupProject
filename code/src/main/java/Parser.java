7804fcef9c1bd28bcb3094a3412f3a5baf247a8e
Joseph Murtha <josephmurtha@gmail.com>
2025-09-22 17:25:48 -0500

    Added test Main method to parser

--- code/src/main/java/Parser.java
+++ code/src/main/java/Parser.java
@@ -18,5 +18,14 @@ public class Parser {
 		// Just so I won't get an error
         return new Order(orderId, type, date, foodItems);
 	}
+
+
+    /**
+     * Main test method for parser class
+     */
+    public static void main(String[] args){
+        Order myOrder = Parser.parseJSONOrder("code/src/main/java/Resources/order_09-16-2025_10-00.json", 12);
+        System.out.println(myOrder);
+    }
 }
 		
