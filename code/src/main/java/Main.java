a54c63c9dbc6f2ada14416f130cedd78b4c78e30
Ashley Zenzola <175030924+azenzola8@users.noreply.github.com>
2025-09-27 20:15:27 -0500

    Added comments.

--- code/src/main/java/Main.java
+++ code/src/main/java/Main.java
@@ -2,7 +2,9 @@ package main.java;
 
 import main.gui.SystemGUI;
 
-// Main class to run the order management system. Launches a simple GUI for user interaction.
+/**
+ * Launches the program and opens the GUI for user interaction.
+ */
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
